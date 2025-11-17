4a2ccbb7800189bb56ba9b3d2153925b69d997f4
yofabronecoforo <60903639+yofabronecoforo@users.noreply.github.com>
2025-11-17 10:50:07 -0800

    Establish basic framework
    - Create preliminary .modinfo
    - Create HostileVillagers.lua gameplay script

---
+++ Scripts/HostileVillagers.lua
@@ -0,0 +1,12 @@
+--[[ =========================================================================
+	Hostile Goody Hut Rewards (HGHR) for Civilization VI
+    Copyright © 2020-2025 yofabronecoforo
+    All rights reserved.
+
+    Begin HostileVillagers.lua gameplay script
+=========================================================================== ]]
+print("Loading gameplay script . . .");
+
+--[[ =========================================================================
+	End HostileVillagers.lua gameplay script
+=========================================================================== ]]

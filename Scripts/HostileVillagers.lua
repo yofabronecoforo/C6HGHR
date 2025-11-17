141fe6aec5238240d398e590b374c4c6f26c97d2
yofabronecoforo <60903639+yofabronecoforo@users.noreply.github.com>
2025-11-17 15:12:51 -0800

    Update gameplay script
    - Stub OnGoodyHutReward() handler for GoodyHutReward event
    - Add InitEventHandlers()
    - Add Initialize()
    - Call InitEventHandlers() from Initialize()
    - Hook Initialize() to LoadScreenClose event

--- Scripts/HostileVillagers.lua
+++ Scripts/HostileVillagers.lua
@@ -5,7 +5,42 @@
 
     Begin HostileVillagers.lua gameplay script
 =========================================================================== ]]
-print("Loading gameplay script . . .");
+g_sRuleset = GameConfiguration.GetValue("RULESET");
+print(string.format("Loading gameplay script for %s . . .", g_sRuleset));
+
+--[[ =========================================================================
+	
+=========================================================================== ]]
+function HostileVillagers_OnGoodyHutReward( player:number, unit:number, goodyhut:number, subtype:number ) 
+    local event:string = "Events.GoodyHutReward:";
+    local bIsHostile:boolean = (GameInfo.GoodyHuts[goodyhut].GoodyHutType == "GOODYHUT_HOSTILES");
+    print(event, player, unit, goodyhut, subtype, bIsHostile);
+    return;
+end
+
+--[[ =========================================================================
+	
+=========================================================================== ]]
+function HostileVillagers_InitEventHandlers() 
+    print("Configuring handler for Events.GoodyHutReward . . .");
+    Events.GoodyHutReward.Add(HostileVillagers_OnGoodyHutReward);
+    return;
+end
+
+--[[ =========================================================================
+	
+=========================================================================== ]]
+function HostileVillagers_Initialize() 
+    print(string.format("Finalizing configuration for %s . . .", g_sRuleset));
+    HostileVillagers_InitEventHandlers();
+    return;
+end
+
+--[[ =========================================================================
+	hook HostileVillagers_Initialize() to LoadScreenClose
+=========================================================================== ]]
+print("Deferring additional configuration to Events.LoadScreenClose.");
+Events.LoadScreenClose.Add(HostileVillagers_Initialize);
 
 --[[ =========================================================================
 	End HostileVillagers.lua gameplay script
